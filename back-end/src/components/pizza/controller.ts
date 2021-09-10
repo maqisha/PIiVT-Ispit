ea48b2f8b830df82a628f04e02217bb00243992d
maqi <maqyx998@gmail.com>
2021-09-10 07:44:01 +0200

    Implement automatic photo resizing

--- back-end/src/components/pizza/controller.ts
+++ back-end/src/components/pizza/controller.ts
@@ -8,6 +8,8 @@ import CFG from "../../config/dev";
 import { v4 as uuid } from 'uuid';
 import { UploadedFile } from "express-fileupload";
 import sizeOf from "image-size";
+import * as path from "path";
+import * as sharp from "sharp";
 
 export default class PizzaController extends BaseController {
     public async getAll(req: Request, res: Response, next: NextFunction) {
@@ -52,28 +54,28 @@ export default class PizzaController extends BaseController {
         const size = sizeOf(file.tempFilePath);
         const limits = CFG.fileupload.photos.limits
 
-        if(size.width < limits.minWidth) {
+        if (size.width < limits.minWidth) {
             return {
                 isOk: false,
                 message: `The image must have a width of at least ${limits.minWidth}px`
             }
         }
 
-        if(size.width > limits.maxWidth) {
+        if (size.width > limits.maxWidth) {
             return {
                 isOk: false,
                 message: `The image must have a width less than ${limits.maxWidth}px`
             }
         }
 
-        if(size.height < limits.minHeight) {
+        if (size.height < limits.minHeight) {
             return {
                 isOk: false,
                 message: `The image must have a height of at least ${limits.minHeight}px`
             }
         }
 
-        if(size.height > limits.maxHeight) {
+        if (size.height > limits.maxHeight) {
             return {
                 isOk: false,
                 message: `The image must have a height less than ${limits.maxHeight}px`
@@ -82,6 +84,29 @@ export default class PizzaController extends BaseController {
 
         return {
             isOk: true,
+        }
+    }
+
+    private async resizeUploadedPhoto(imagePath: string) {
+        const pathInfo = path.parse(imagePath);
+
+        for (const resize of CFG.fileupload.photos.resizes) {
+            const resizedImagePath = pathInfo.dir + "/" + pathInfo.name + resize.sufix + pathInfo.ext;
+
+            await sharp(imagePath)
+                .resize({
+                    width: resize.width,
+                    height: resize.height,
+                    fit: resize.fit,
+                    background: {
+                        r: 255,
+                        g: 255,
+                        b: 255,
+                        alpha: 1.0,
+                    },
+                    withoutEnlargement: true,
+                })
+                .toFile(resizedImagePath)
         }
     }
 
@@ -94,7 +119,7 @@ export default class PizzaController extends BaseController {
         const file = req.files[Object.keys(req.files)[0]] as any;
 
         const photoValidation = this.isValidPhoto(file);
-        if(!photoValidation.isOk) {
+        if (!photoValidation.isOk) {
             res.status(400).send(photoValidation.message);
             return null;
         }
@@ -107,6 +132,7 @@ export default class PizzaController extends BaseController {
             uuid() + "-" + file?.name;
 
         await file.mv(imagePath);
+        await this.resizeUploadedPhoto(imagePath);
         return imagePath;
     }
 
@@ -116,17 +142,22 @@ export default class PizzaController extends BaseController {
             return;
         }
 
-        const data = JSON.parse(req.body?.data);
-        data.imagePath = imagePath;
+        try {
+            const data = JSON.parse(req.body?.data);
+            data.imagePath = imagePath;
 
-        if (!IAddPizzaValidator(data)) {
-            res.status(400).send(IAddPizzaValidator.errors);
-            return;
+            if (!IAddPizzaValidator(data)) {
+                res.status(400).send(IAddPizzaValidator.errors);
+                return;
+            }
+
+            const result: PizzaModel | IErrorResponse = await this.services.pizzaService.add(data as IAddPizza);
+
+            res.send(result);
+        } catch (err) {
+            res.send(400).send(err?.message);
         }
 
-        const result: PizzaModel | IErrorResponse = await this.services.pizzaService.add(data as IAddPizza);
-
-        res.send(result);
     }
 
     public async edit(req: Request, res: Response, next: NextFunction) {
